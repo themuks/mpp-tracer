fa61275c11500ae9493852ec031ea8a7fbb4927e
Maksim Kuntsevich <max-cun4@yandex.ru>
2020-10-04 22:37:08 +0300

    feat: add xml serializer

---
+++ main/serializer/impl/XmlSerializerImpl.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
+using tracer;
+
+namespace mpp_tracer
+{
+    public class XmlSerializer : ISerializer
+    {
+        public byte[] Serialize(object o)
+        {
+            DataContractSerializer serializer = new DataContractSerializer(typeof(object));
+            XmlWriterSettings xmlWriterSettings = new XmlWriterSettings()
+            {
+                Indent = true
+            };
+            MemoryStream memoryStream = new MemoryStream();
+            using (XmlWriter xmlWriter = XmlWriter.Create(memoryStream, xmlWriterSettings))
+            {
+                serializer.WriteObject(xmlWriter, o);
+            }
+            return memoryStream.GetBuffer();
+        }
+    }
+}
